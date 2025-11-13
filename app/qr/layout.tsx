391956c91586f7864d1383073178292cf365cccf
Oskari Järvelin <contact@oskarijarvelin.fi>
2025-11-13 23:17:07 +0200

    Removed background-image

--- app/qr/layout.tsx
+++ app/qr/layout.tsx
@@ -1,12 +1,6 @@
-import Header from "@/components/molecules/header";
-import ImageBackground from "@/components/atoms/image-background";
-import MaxWidthWrapper from "@/components/atoms/max-width-wrapper";
-
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-harmaa dark:bg-yotaivas xs:py-10 md:py-12 w-full h-full min-h-screen transition relative">
-      {/* BACKGROUND PATTERN  */}
-      <ImageBackground />
+    <div className="bg-valkoinen dark:bg-yotaivas xs:py-10 md:py-12 w-full h-full min-h-screen transition relative">
       <main className="h-full xl:h-auto flex items-center justify-center">{children}</main>
     </div>
   );
--- app/tulokset/layout.tsx
+++ app/tulokset/layout.tsx
@@ -1,12 +1,9 @@
 import Header from "@/components/molecules/header";
-import ImageBackground from "@/components/atoms/image-background";
 import MaxWidthWrapper from "@/components/atoms/max-width-wrapper";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-valkoinen dark:bg-yotaivas xs:py-10 md:py-12 w-full h-full min-h-screen transition relative">
-      {/* BACKGROUND PATTERN  */}
-      <ImageBackground />
       <MaxWidthWrapper className="flex justify-end xs:mb-10 lg:mb-2 xs:px-5 md:px-0">
         <Header />
       </MaxWidthWrapper>
